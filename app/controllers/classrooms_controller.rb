a0bb016374abf56c8d421a81e9cea43dd230b13f
Srija S <srijakumar7@gmail.com>
2019-06-01 22:53:16 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,11 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
+<<<<<<< HEAD
     #@student = @classroom.oldest_student
+=======
+  
+>>>>>>> a81b5a46713a12328ed000345d3dfdbabf1551fd
   end
 
   #def index
